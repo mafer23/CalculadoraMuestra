7199f566f96b5d28aa610de70e468449137b89b5
mafer23 <mafer18199420@gmail.com>
2023-10-16 18:27:40 -0500

    creo que termine por fin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MathjaxModule } from 'mathjax-angular';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+
     CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MathjaxModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-card',
@@ -10,6 +12,15 @@ export class CardComponent {
   calculoForm!: FormGroup;
   resultado!: string;
   sum!: number;
+  cantidadPob!:string;
+  checkBoxMarcado = false;
+  formulaDiv: any;
+  content: string = ''; // Inicializa con un valor si es necesario
+  content2: string = ''; // Inicializa con un valor si es necesario
+
+  content1 = document.getElementById('content1') as HTMLDivElement;
+  content23 = document.getElementById('content2') as HTMLParagraphElement;
+
 
 
   constructor(private readonly fb: FormBuilder){
@@ -18,24 +29,27 @@ export class CardComponent {
 
   ngOnInit(): void {
     this.calculoForm = this.fb.group({
-      poblationForm: ['opcion1', [Validators.required]],
+      poblationForm: ['', [Validators.required]],
        nivel:['',[Validators.required , Validators.pattern(/^[0-9]*$/ )]],
       precision: ['',[Validators.required ,Validators.pattern(/^[0-9]*$/) ]],
       cantPoblacion: ['',[Validators.required ,Validators.pattern(/^[0-9]*$/)]]
      })
    
 
+
+
+
        this.calculoForm.get('poblationForm')?.valueChanges.subscribe((value: any) =>{
-      const cantPoblacionControl = this.calculoForm.get('cantPoblacion');
- 
-      if (cantPoblacionControl) {
-          if (value) {
-            cantPoblacionControl.enable(); // Habilitar el campo
-          } else {
-             cantPoblacionControl.disable(); // Deshabilitar el campo
-          }
-       }
-   })
+
+
+        if(value == "opcion2"){
+
+          this.calculoForm.get('cantPoblacion')?.disable();
+        } else{
+          this.calculoForm.get('cantPoblacion')?.enable();
+
+        }
+      })
  
   }
 
@@ -53,7 +67,7 @@ export class CardComponent {
   }
 
 
-
+ 
   
 
   onSubmit(){
@@ -61,20 +75,30 @@ export class CardComponent {
    let N= this.cantPoblaControl?.value;
    let Z = this.nivelControl?.value;
    let e = this.precisionControl?.value;
+   const contElement = document.getElementById("cont");
+
  
   if(this.calculoForm.get('poblationForm')?.value == 'opcion1'){
      
     if(this.nivelControl?.value == 95){
 
       // const suma = Math.pow(2,2)
+      
+      Z=  1.96
+   
       const suma = (N*Math.pow(1.96,2)*(0.5)*(0.5))/(Math.pow(e/100,2)*(N-1)+Math.pow(1.96,2)*(0.5)*(0.5))
-      console.log(suma )
+      this.content = `$x = {${N}*(${Z}^2)(0.5)(0.5) \\over (\\frac{${e}}{100})^2 (${N}-1)+ (${Z}^2)(0.5)(0.5)}$`  
+      this.content2 = "="+suma.toString()
+
+     
       
     } else{
 
+      Z=  2.58
       const suma = (N*Math.pow(2.58,2)*(0.5)*(0.5))/(Math.pow(e/100,2)*(N-1)+Math.pow(2.58,2)*(0.5)*(0.5))
-      console.log(suma )
-      console.log("Buenas")
+      this.content = `$x = {${N}*(${Z}^2)(0.5)(0.5) \\over (\\frac{${e}}{100})^2 (${N}-1)+ (${Z}^2)(0.5)(0.5)}$`  
+      this.content2 = "="+suma.toString()
+  
 
     }
     
@@ -85,18 +109,25 @@ export class CardComponent {
     if(this.nivelControl?.value == 95){
 
       // const suma = Math.pow(2,2)
+      Z = 1.96
       const suma = (Math.pow(1.96,2)*(0.5)*(0.5))/(Math.pow(e/100,2))
-      console.log(suma )
+      this.content = `$x = {(${Z}^2)(0.5)(0.5) \\over (${e})^2}$`  
+      this.content2 = "="+suma.toString()
+
+
       
     } else{
 
+      Z = 2.58
       const suma = (Math.pow(2.58,2)*(0.5)*(0.5))/(Math.pow(e/100,2))
-      console.log(suma )
-      console.log("poblacion infinita")
+      this.content = `$x = {(${Z}^2)(0.5)(0.5) \\over (${e})^2}$`  
+      this.content2 = "="+suma.toString()
 
     }
     
   }
+
+
 
 
     
@@ -107,9 +138,24 @@ export class CardComponent {
       console.log(opcionSeleccionada)
       console.log('Form ->', this.calculoForm.value)
     } else {
-      this.resultado = "No son validos";
-      console.log('No son validos')
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...no puedes seguir',
+        text: 'Llena los campos por favor',
+      })
+    
     }
+
+  }
+
+  limpiar(){
+
+    this.calculoForm.reset();
+    this.content = '';
+    this.content2 = '';
+    this.content1.textContent = '';
+    this.content23.textContent = '';
+
 
   }
 
